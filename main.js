f9c9672a88f5fcf6c0720058e40795cd2e2c393e
20a3133 <20a3133@sit.shonan-it.ac.jp>
2023-05-17 17:22:51 +0900

    update

---
+++ main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 600,
+        height: 400,
+        title: 'マイアプリ',
+    });
+    mainWindow.loadFile('index.html');
+};
+
+app.once('ready', () => {
+    createWindow();
+});
